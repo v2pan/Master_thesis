7829a5dc2540c1cbc8506c5ad991f8510ed29736
v2pan <v.panin2000@web.de>
2025-02-10 17:55:20 +0100

    New dataset files

--- PLAY.py
+++ PLAY.py
@@ -1,2 +1,8 @@
-import math
-print(int(math.sqrt(5)))
+from database import query_database
+
+print(query_database('''
+SELECT airportname, city, shortname
+FROM airport
+LEFT JOIN airport_transl ON airport.airportname=airport_transl.fullname
+LEFT JOIN airport_abrev ON airport_abrev.name=airport_transl.shortname;
+'''))
