e5262835a2c2d800317ebcd20e588a7f438c4679
Vladimir Panin <93733063+v2pan@users.noreply.github.com>
2026-01-19 22:26:48 +0100

    Comment out inital execution
    Comment out the call to combined_pipeline for dog category query.

--- Main/combined_pipeline.py
+++ Main/combined_pipeline.py
@@ -286,6 +286,6 @@ def hard_pipeline(query):
             initial_query_result=[]
         return initial_query_result, temp_meta
 
-combined_pipeline("SELECT * FROM animalowner WHERE animalowner.category = 'dog' ",  evaluation=True)
+#combined_pipeline("SELECT * FROM animalowner WHERE animalowner.category = 'dog' ",  evaluation=True)
 
 
